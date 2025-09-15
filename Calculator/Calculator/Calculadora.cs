1cda955a10f59d6a5f2b278c0d8caa4ea26b5f83
Willian Classen <Wgclassen@gmail.com>
2025-09-14 22:00:24 -0300

    first commit

---
+++ Calculator/Calculator/Calculadora.cs
@@ -0,0 +1,19 @@
+﻿namespace Calculator;
+
+public class Calculadora
+{
+    public int Somar(int numero1, int numero2)
+    {
+        return (numero1 + numero2);
+    }
+
+    public int Somar(int numero1, int numero2, int numero3)
+    {
+        return (numero1 + numero2 + numero3);
+    }
+
+    public double Somar(double numero1, double numero2)
+    {
+        return (numero1 + numero2);
+    }
+}
---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,6 @@
+﻿using Calculator;
+
+Calculadora calc = new Calculadora();
+Console.WriteLine(calc.Somar(10, 20));
+Console.WriteLine(calc.Somar(5, 10, 15));
+Console.WriteLine(calc.Somar(3.5, 2.8));
